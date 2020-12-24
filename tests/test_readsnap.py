017f5ab09cf5c03ace958ae7f5ccd8635b4b702d
tsukanov-as <tsukanov.as@yandex.ru>
2020-12-24 16:09:37 +0300

    Add python3 support

--- tests/test_readsnap.py
+++ tests/test_readsnap.py
@@ -1,7 +1,7 @@
 import tarantool17_snapshot
 import msgpack
 from unittest import TestCase
-import fixtures
+import tests.fixtures as fixtures
 
 class TestSnapshot(TestCase):
     def test_read_v12(self):
@@ -10,16 +10,16 @@ class TestSnapshot(TestCase):
 
         try:
             for meta_data, row_data in tarantool17_snapshot.iter("testdata/v12/00000000000000000000.ok.snap"):
-                meta = msgpack.unpackb(meta_data)
-                row = msgpack.unpackb(row_data)
+                meta = msgpack.unpackb(meta_data, strict_map_key=False, raw=False)
+                row = msgpack.unpackb(row_data, strict_map_key=False, raw=False)
 
                 metas.append(meta)
                 rows.append(row)
         except:
             self.fail("Reading v12 snapshot failed")
 
-        self.assertEquals(fixtures.v12_metas, metas)
-        self.assertEquals(fixtures.v12_rows, rows)
+        self.assertEqual(fixtures.v12_metas, metas)
+        self.assertEqual(fixtures.v12_rows, rows)
 
     def test_read_v12_header(self):
         header = {}
@@ -29,7 +29,7 @@ class TestSnapshot(TestCase):
         except:
             self.fail("Reading v12 snapshot header failed")
 
-        self.assertEquals(fixtures.v12_header, header)
+        self.assertEqual(fixtures.v12_header, header)
 
     def test_read_v13(self):
         metas = []
@@ -37,16 +37,16 @@ class TestSnapshot(TestCase):
 
         try:
             for meta_data, row_data in tarantool17_snapshot.iter("testdata/v13/00000000000000000000.ok.snap"):
-                meta = msgpack.unpackb(meta_data)
-                row = msgpack.unpackb(row_data)
+                meta = msgpack.unpackb(meta_data, strict_map_key=False, raw=False)
+                row = msgpack.unpackb(row_data, strict_map_key=False, raw=False)
 
                 metas.append(meta)
                 rows.append(row)
         except:
             self.fail("Reading v13 snapshot failed")
 
-        self.assertEquals(fixtures.v13_metas, metas)
-        self.assertEquals(fixtures.v13_rows, rows)
+        self.assertEqual(fixtures.v13_metas, metas)
+        self.assertEqual(fixtures.v13_rows, rows)
 
     def test_read_v13_header(self):
         header = {}
@@ -56,42 +56,42 @@ class TestSnapshot(TestCase):
         except:
             self.fail("Reading v13 snapshot header failed")
 
-        self.assertEquals(fixtures.v13_header, header)
+        self.assertEqual(fixtures.v13_header, header)
 
     def test_bigsnap_v13(self):
         count = 0
         try:
             for meta_data, row_data in tarantool17_snapshot.iter("testdata/v13/00000000000000010005.ok.snap"):
-                msgpack.unpackb(meta_data)
-                msgpack.unpackb(row_data)
+                msgpack.unpackb(meta_data, strict_map_key=False, raw=False)
+                msgpack.unpackb(row_data, strict_map_key=False, raw=False)
 
                 count = count + 1
         except:
             self.fail("Reading v13 snapshot failed")
 
-        self.assertEquals(count, 10511)
+        self.assertEqual(count, 10511)
 
     def test_corrupted_zstd_v13(self):
         with self.assertRaises(tarantool17_snapshot.SnapshotError) as ctx:
             for _, _ in tarantool17_snapshot.iter("testdata/v13/corr.block.snap"):
                 pass
-        self.assertEquals(str(ctx.exception), "Error reading 'testdata/v13/corr.block.snap': zstd error: Corrupted block detected")
+        self.assertEqual(str(ctx.exception), "Error reading 'testdata/v13/corr.block.snap': zstd error: Corrupted block detected")
 
     def test_no_eof_v13(self):
         with self.assertRaises(tarantool17_snapshot.SnapshotError) as ctx:
             for _, _ in tarantool17_snapshot.iter("testdata/v13/no.eof.snap"):
                 pass
-        self.assertEquals(str(ctx.exception), "Error reading 'testdata/v13/no.eof.snap': truncated stream")
+        self.assertEqual(str(ctx.exception), "Error reading 'testdata/v13/no.eof.snap': truncated stream")
 
     def test_bad_version_xlog(self):
         with self.assertRaises(tarantool17_snapshot.SnapshotError) as ctx:
             for _, _ in tarantool17_snapshot.iter("testdata/version.bad.xlog"):
                 pass
-        self.assertEquals(str(ctx.exception), "Error opening 'testdata/version.bad.xlog': unknown header version: 0.07\n")
+        self.assertEqual(str(ctx.exception), "Error opening 'testdata/version.bad.xlog': unknown header version: 0.07\n")
 
     def test_bad_format(self):
         with self.assertRaises(tarantool17_snapshot.SnapshotError) as ctx:
             for _, _ in tarantool17_snapshot.iter("testdata/format.bad.xlog"):
                 pass
-        self.assertEquals(str(ctx.exception), "Error opening 'testdata/format.bad.xlog': unknown file header: expected SNAP or XLOG")
+        self.assertEqual(str(ctx.exception), "Error opening 'testdata/format.bad.xlog': unknown file header: expected SNAP or XLOG")
 
